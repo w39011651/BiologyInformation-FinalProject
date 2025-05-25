7a01051a7d63c6c2865ebf83744453aae47b0bae
yh0428 <imdavidouo@gmail.com>
2025-05-25 22:40:04 +0800

    predict的input改成list of tuple

--- app/cnn_with_bm/runner.py
+++ app/cnn_with_bm/runner.py
@@ -7,6 +7,6 @@ def run(protein_information):
     seq = extract_fasta_sequence(protein_information)
     primary_accession = extract_primary_accession(protein_information)
     model = load_model()
-    results = predict(model, [primary_accession, seq])#先暫定只會輸入一個Fasta file
+    results = predict(model, [(primary_accession, seq)])#先暫定只會輸入一個Fasta file
 
     return output(results)
